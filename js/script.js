6d093a50a18c4e36838fdd177b35d8bca71a0840
Matheus Arthur <matheus.artsilva@gmail.com>
2025-08-13 16:30:01 -0300

    learn(ap01-js): encerra o codigo 1.6.3
    - lidamos com vetores
    - lidamos com funções
    - retomar lidando com closures

--- js/script.js
+++ js/script.js
@@ -1,3 +1,83 @@
+//vetores, parte 2
+//mapreduce
+// const valores = [1, 2, 3, 4]
+// const soma = valores.reduce((ac, v) => ac + v)
+// console.log(soma)
+
+// const nomes = ['Ana Maria', 'Antonio', 'Rodrigo', 'Alex', 'Cristina']
+// const todosComecamComA = nomes.every(n => n.startsWith('A'))
+// console.log(todosComecamComA)
+// const peloMenosAlgumComecaComA = nomes.some(n => n.startsWith('A'))
+// console.log(peloMenosAlgumComecaComA)
+//dada a coleção a seguir, produzir outra coleção contendo o quadrado de cada número
+//usando arrow functions e digitando a menor quantidade de caracteres possível
+// const numeros = [1, 2, 3] //[1, 4, 9]
+// console.log(numeros.map(n => n * n))
+//['Ana Maria', 'Antonio', 'Rodrigo', 'Alex', 'Cristina'] -> ['A', 'A', 'R', 'A', 'C']
+// const res = nomes.map(function(nome){return nome[0]})
+// console.log(res)
+// const apenasComA = nomes.filter(nome => nome.startsWith('A'))
+// console.log(apenasComA)
+//arrow function
+//tem exatamente um parâmetro: parênteses podem ser omitidos:
+// const dobro = n => 2 * n
+// console.log(dobro(2))
+//tem somente uma linha que produz um valor a ser devolvido:
+// const dobro = (n) => { 
+//     console.log('Calculando o dobro...')
+//     return 2 * n 
+// }
+// console.log(dobro(1))
+// const dobro = (n) => 2 * n
+// console.log(dobro(5))
+
+//tem somente uma linha: as chaves podem ser omitidas
+// const hello = () => console.log('Hello')
+// hello()
+// const hello = () => {
+//     console.log('Hello')
+// }
+// hello()
+// const triplo = function(n = 5){
+//     return 3 * n
+// }
+// console.log(triplo())
+// console.log(triplo(10))
+
+// const dobro = function (n) {
+//     return 2 * n
+// }
+// console.log(dobro(6))
+///callable "chamável", que pode ser chamado
+
+// function soma(a, b){
+//     return a + b
+// }
+// const res = soma(2, 3)
+// console.log(res)
+// function hello(){
+//     console.log(`Oi`)
+// }
+// hello()
+// function hello(nome){
+//     console.log('Hello, ' + nome)
+// }
+// hello('Ana')
+
+//vetores
+// v2 = [2, "abc", true]
+// console.log(v2)
+// for (let i = 0; i < v2.length; i++){
+//     console.log(v2[i])
+// }
+// v1 = []
+// console.log(v1.length)
+// v1[0] = 3.4
+// console.log (v1.length)
+// v1[10] = 2
+// console.log(v1.length)
+// v1[7] = "abc"
+
 //comparação
 //operadores == e ===
 // [] ArrayList<Object>();
@@ -9,22 +89,25 @@
 // b = 2
 // b = 'abc'
 // if(b == a{
-
 // })
 // console.log(1 == [1])
 // console.log(true == "true")
 // console.log(1 === '1')
 // console.log(2 == '2')
 // console.log(1 == 1)
+
 //coerção
 // const n1 = 2
 // const n2 = '3'
+
 // //coerção implícita
 // const n3 = n1 + n2
 // console.log(n3)
+
 // //coerção explícita
 // const n4 = n1 + Number(n2)
 // console.log(n4)
+
 //sistema de tipos
 // let idade = 20
 // console.log(typeof(idade))
@@ -32,6 +115,7 @@
 // console.log(typeof(nome))
 // idade = "vinte"
 // console.log(typeof(idade))
+
 //declaração de variáveis
 //const, let e var
 // var idade = 18
