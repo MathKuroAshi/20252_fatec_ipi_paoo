e8372c6b9de7885715886f379bf16b917401c847
Matheus Arthur <matheus.artsilva@gmail.com>
2025-09-03 18:20:21 -0300

    learn(ap01-js): encerra a apostila

--- js/previsao_do_tempo/index.js
+++ js/previsao_do_tempo/index.js
@@ -1,22 +1,124 @@
+require('dotenv').config()
 const axios = require('axios')
 
-const PROTOCOL = 'https'
-const BASE_URL = 'api.openweathermap.org/data/2.5/forecast'
-const APPID = ''
-const UNITS = 'metric'
-const Q = 'Itu'
-const LANG = 'pt_br'
-const CNT = 1
 
-const URL = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&units=${UNITS}&q=${Q}&lang=${LANG}&cnt=${CNT}`
 
-const promiseResultante = axios.get(URL)
 
-promiseResultante
-.then((previsoes) => {
-    console.log(previsoes)
-})
-.catch((erro) => {
-    console.log(`Erro: ${erro}`)
-})
-console.log('Terminando...')
+// const PROTOCOL = process.env.PROTOCOL
+// const BASE_URL = process.env.BASE_URL
+// const APPID = process.env.APPID
+// const UNITS = process.env.UNITS
+// const Q = process.env.Q
+// const LANG = process.env.IDIOM
+// const CNT = process.env.CNT
+
+//async/await
+function fatorial(n){
+    if(n < 0) return Promise.reject('Valor não pode ser negativo')
+    let res = 1
+    for(let i = 2; i <= n; i++) res *= i
+    return Promise.resolve(res)
+
+}
+
+async function chamadaComAsyncAwait(){
+    const n1 = 5, n2 = -2;
+    try{
+        const res1 = await fatorial(n1)
+        console.log(`Fatorial de ${n1}: ${res1}`)
+    } catch(err){
+        console.log(`Erro: ${err}`)
+    }
+
+    try {
+        const res2 = await fatorial(n2)
+        console.log(`Fatorial de ${n2}: ${res2}`)
+    } catch (erro) {
+        console.log(`Erro: ${erro}`)
+    }
+}
+
+chamadaComAsyncAwait()
+console.log('Terminando script principal...')
+
+// function chamadaComThenECatch(){
+//     const n1 = 5, n2 = -2;
+//     fatorial(n1)
+//     .then(res => console.log(`Fatorial de ${n1}: ${res}`))
+//     .catch(err => console.log(`Erro: ${err}`))
+
+//     fatorial(n2)
+//     .then(res => console.log(`Fatorial de ${n2}: ${res}`))
+//     .catch(err => console.log(`Erro: ${err}`))
+// }
+
+// chamadaComThenECatch()
+
+// async function hello(nome){
+//     return `Oi, ${nome}`
+// }
+// hello("Ana").then(res => console.log(res))
+// function hello(nome){
+//     // return `Oi, ${nome}`
+//     const p = new Promise((resolve, reject) => {
+//         resolve(`Oi, ${nome}`)
+//     })
+//     return p
+// }
+// const res = hello('Ana')
+// res.then((resposta) => {
+//     console.log(resposta)
+// })
+
+
+
+// const { 
+//     PROTOCOL,
+//     BASE_URL,
+//     APPID,
+//     UNITS,
+//     Q,
+//     IDIOM: LANG,
+//     CNT } = process.env
+
+// const URL = `${PROTOCOL}://${BASE_URL}?appid=${APPID}&units=${UNITS}&q=${Q}&lang=${LANG}&cnt=${CNT}`
+
+// console.log(URL)
+
+// const promiseResultante = axios.get(URL)
+
+// promiseResultante
+// .then((resposta) => {
+    //ajustar o código para que obtenhamos 3 previsões. Iterar sobre as previsões e, para cada uma, exibir:
+    //temp min
+    //temp max
+    //umidade relativa do ar
+    //descrição
+//     for (const previsao of resposta.data.list) {
+//         console.log(previsao.main.temp_min)    
+//         console.log(previsao.main.temp_max)    
+//         console.log(previsao.main.humidity)    
+//         console.log(previsao.weather[0].description)    
+//     }
+//     return resposta
+// })
+// .then((resposta) => {
+//     //console.log(resposta)
+//     const list = resposta.data.list
+//     console.log(list)
+//     return list
+//  })
+// .then((resposta) => {
+    //exibir temperatura mínima e máxima da primeira previsão
+//     console.log(`Temp min: ${resposta[0].main.temp_min}`)
+//     console.log(`Temp max: ${resposta[0].main.temp_max}`)
+//     return resposta[0].weather
+// })
+// .then((resposta) => {
+//     console.log(resposta[0].description)
+//     })
+// .catch((erro) => {
+//     console.log(`Erro: ${erro}`)
+// })
+
+// console.log('Terminando...')
