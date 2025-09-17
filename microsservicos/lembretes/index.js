587a76d490c5bf2eedb97ab482ebe519e547638b
Matheus Arthur <matheus.artsilva@gmail.com>
2025-09-17 18:09:41 -0300

    learn(ap01-js): encerra o codigo 4.3.10
    - semana que vem tem prova
    - retomar testando os endpoints de crud de observacoes

---
+++ microsservicos/lembretes/index.js
@@ -0,0 +1,38 @@
+// Endpoints: método de protocolo http, padrão de acesso e funcionalidade
+// API: Coleção de endpoints
+const express = require('express')
+const app = express()
+app.use(express.json())
+//GET serve para obter coisas do servidor
+//POST serve para cadastrar itens novos
+//PUT serve para atualizar itens existentes
+//DELETE serve para apagar itens existentes
+//Um lembrete: {id: 1, texto: 'Fazer café}
+/*
+    lembretes:
+    {
+        1: {id: 1, texto: 'Fazer café},
+        2: {id: 2, texto: 'Ir à feira'}
+    }
+*/
+let id = 0
+const lembretes = {}
+//definindo um endpoint que permite que lembretes sejam cadastrados
+//POST /lembretes (req, res) => {}
+app.post('/lembretes', function(req, res){
+    id++
+    const texto = req.body.texto
+    // const lembrete = { id: id, texto: texto }
+    const lembrete = { id, texto}
+    lembretes[id] = lembrete
+    res.status(201).json(lembrete)
+})
+
+//definindo um endpoint que permite que a coleção de lembretes seja obtida
+//GET /lembretes (req, res) => {}
+app.get('/lembretes', (req, res) => {
+    res.json(lembretes)
+})
+
+const port = 4000
+app.listen(4000, () => console.log(`Lembretes. Porta ${port}.`))
